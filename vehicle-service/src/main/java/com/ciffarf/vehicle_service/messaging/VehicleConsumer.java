b89b935ee98a6ce0762658a694ec62024e259713
Anuj Pal <anujpal2454@gmail.com>
2026-06-01 16:48:07 +0530

    git commit -m "Added notification service and RabbitMQ microservice communication"

--- vehicle-service/src/main/java/com/ciffarf/vehicle_service/messaging/VehicleConsumer.java
+++ vehicle-service/src/main/java/com/ciffarf/vehicle_service/messaging/VehicleConsumer.java
@@ -1,23 +1,23 @@
-package com.ciffarf.vehicle_service.messaging;
-
-
-import com.ciffarf.vehicle_service.config.RabbitMQConfig;
-import com.ciffarf.vehicle_service.event.VehicleCreatedEvent;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Component;
-
-@Component
-@Slf4j
-public class VehicleConsumer {
-    @RabbitListener(
-            queues = RabbitMQConfig.QUEUE)
-    public void consumeVehicleCreatedEvent(
-            String message){
-
-        log.info(
-                "Received Message {}",
-                message);
-    }
-
-}
+//package com.ciffarf.vehicle_service.messaging;
+//
+//
+//import com.ciffarf.vehicle_service.config.RabbitMQConfig;
+//import com.ciffarf.vehicle_service.event.VehicleCreatedEvent;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.amqp.rabbit.annotation.RabbitListener;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@Slf4j
+//public class VehicleConsumer {
+//    @RabbitListener(
+//            queues = RabbitMQConfig.QUEUE)
+//    public void consumeVehicleCreatedEvent(
+//            String message){
+//
+//        log.info(
+//                "Received Message {}",
+//                message);
+//    }
+//
+//}
